5d278ef49b93717a47dd69aad09769afc24d5cfd
METEHANBYRKDR <102472877+METEHANBYRKDR@users.noreply.github.com>
2024-03-06 16:03:27 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICustomerDal.cs
@@ -0,0 +1,7 @@
+﻿using Entities.Concrete;
+
+namespace DataAccess.Abstract;
+
+public interface ICustomerDal:IEntityRepository<Customer>
+{
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System.Linq.Expressions;
+
+namespace DataAccess.Abstract;
+
+// generic constraint
+// IEntity veya IEntity İmplementasyonlarını alabilir.
+// IEntity newlenemiyiceği için IEntity koyamayız
+public interface IEntityRepository<T> where T : class,IEntity, new()
+{
+    List<T> GetAll(Expression<Func<T,bool>> filter = null);
+    T Get(Expression<Func<T,bool>> filter);
+
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
